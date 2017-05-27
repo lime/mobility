766d958b2c0a2260e017be04029a3606aa6b0521
Chris Salzberg <chris@dejimata.com>
2017-05-27 18:06:00 +0900

    Do not override dirty methods in AR >= 5.1

--- lib/mobility/active_record.rb
+++ lib/mobility/active_record.rb
@@ -12,18 +12,20 @@ Module loading ActiveRecord-specific classes for Mobility models.
     autoload :Translation,         "mobility/active_record/translation"
     autoload :UniquenessValidator, "mobility/active_record/uniqueness_validator"
 
-    def changes_applied
-      @previously_changed = changes
-      super
-    end
+    if ::ActiveRecord::VERSION::MAJOR == 5 && ::ActiveRecord::VERSION::MINOR == 0
+      def changes_applied
+        @previously_changed = changes
+        super
+      end
 
-    def clear_changes_information
-      @previously_changed = ActiveSupport::HashWithIndifferentAccess.new
-      super
-    end
+      def clear_changes_information
+        @previously_changed = ActiveSupport::HashWithIndifferentAccess.new
+        super
+      end
 
-    def previous_changes
-      super.merge(@previously_changed ||= ActiveSupport::HashWithIndifferentAccess.new)
+      def previous_changes
+        super.merge(@previously_changed ||= ActiveSupport::HashWithIndifferentAccess.new)
+      end
     end
 
     def self.included(model_class)
